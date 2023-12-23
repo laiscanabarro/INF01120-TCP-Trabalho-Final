f24a080d41b5db66005bb986a7082bd355231ed4
Lais Canabarro <laisaraujo1312@gmail.com>
2023-12-23 09:08:03 -0300

    add Location class

---
+++ Location.java
@@ -0,0 +1,46 @@
+public class Location {
+    private String city;
+    private String country;
+    private String street;
+    private int number;
+
+    public Location(String city, String country, String street, int number) {
+        this.city = city;
+        this.country = country;
+        this.street = street;
+        this.number = number;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public String getStreet() {
+        return this.street;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+}
