77c89f07c134cd4207aa0b524bb175021a0a259c
Lais Canabarro <laisaraujo1312@gmail.com>
2023-12-23 11:44:22 -0300

    add: Calender

---
+++ Calender.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Calender {
+    ArrayList<Event> events = new ArrayList<>();
+    //ArrayList<Task> tasks = new ArrayList<>();
+
+    //public boolean addEvent(Event event){}
+
+    //public boolean addTask(Task task){}
+
+    //public void blockCalender(){}
+
+    //public void displayCalender(){}
+
+    //public Event searchEvent(String name){}
+
+    //public void selectDay(){}
+
+    //public void selectMonth(){}
+
+    //public void selectEvent(Event event){}
+
+    //public void selectTask(Task task){}
+
+}
