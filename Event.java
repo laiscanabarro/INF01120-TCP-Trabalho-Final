65aefd987fed34ff966475be3c29564a090715e1
Lais Canabarro <laisaraujo1312@gmail.com>
2023-12-23 09:09:25 -0300

    add Event class

---
+++ Event.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+//import java.util.ArrayList;
+
+public class Event {
+    String eventName;
+    Location location; 
+    LocalDate starDate;
+    LocalDate endDate;
+    //ArrayList<User> participants = new ArrayList<>()
+
+    public Event(String name, Location location, LocalDate star, LocalDate end){}
+    //public Event(String name, Location location, LocalDate star, LocalDate end, ArrayList<User> participants){}
+
+    //public void shareWhithFriend(User friend){}
+
+    //public void shareWhithCommunity(Community community){}
+
+    // public boolean creatRecurrence(LocalDate starDate, LocalDate endDate){}
+    
+}
