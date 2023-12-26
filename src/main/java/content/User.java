5529fdf009e82a265915eb7071d4a5ce826c3d7d
Laura Keidann <laura.keidann@sap.com>
2023-12-26 19:57:06 -0300

    Add classes

---
+++ src/main/java/content/User.java
@@ -0,0 +1,45 @@
+package content;
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    
+    private String name;
+    private String email;
+    private String password;
+    private ArrayList<User> friends;
+    private HabitsList habits;
+    private Productivity productivity; 
+
+
+    public void editProfile(){}
+        //  TBD
+
+    public boolean addFriend(User friend){
+        //  TBD
+        return true;
+    }
+
+    public boolean removeFriend(User friend){
+        //  TBD
+        return true;
+    }
+
+    public boolean inviteFriend(User friend){
+        //  TBD
+        return true;
+    }
+
+    public void createCommunity(){}
+        //  TBD
+
+    public void createList(){}
+        //  TBD
+
+    public boolean createUser(){}
+        //  TBD
+
+    public Report generateProductivityReport(){
+        return new Report(productivity);
+    }
+}
