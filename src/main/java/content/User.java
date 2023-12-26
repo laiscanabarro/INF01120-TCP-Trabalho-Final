60a45054fe637328bdf7cd8f91e9cb2da706f88e
Laura Keidann <laura.keidann@sap.com>
2023-12-26 20:20:02 -0300

    Add to correct folder

---
+++ src/main/java/content/User.java
@@ -0,0 +1,63 @@
+package content;
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private String name;
+    private String email;
+    private String password;
+    private ArrayList<User> friends;
+    private HabitsList habits;
+    private Productivity productivity;
+
+    // Constructor
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void editProfile() {
+    }
+    // TBD
+
+    public boolean addFriend(User friend) {
+        // TBD
+        return true;
+    }
+
+    public boolean removeFriend(User friend) {
+        // TBD
+        return true;
+    }
+
+    public boolean inviteFriend(User friend) {
+        // TBD
+        return true;
+    }
+
+    public void createCommunity() {
+    }
+    // TBD
+
+    public void createList() {
+    }
+    // TBD
+
+    public boolean createUser() {
+    }
+    // TBD
+
+    public Report generateProductivityReport() {
+        return new Report(productivity);
+    }
+}
