e434a13416f46ee713e216386c3bd376092391f1
Lais Canabarro <laisaraujo1312@gmail.com>
2023-12-25 23:14:25 -0300

    add: class Invitation

---
+++ Invitation.java
@@ -0,0 +1,46 @@
+public class Invitation {
+    private boolean accept;
+    //private User guest;
+    //private Community community;
+
+    /* 
+    public Invitation(User user){
+        this.accept = false;
+        this.invited = guest;
+    }
+
+    public Invitation(Community community){
+        this.accept = false;
+        User owner = community.owner; //owner é um usuário ou só uma string?
+        this.invited = owner;
+    }
+    */
+
+    public boolean getAccept(){
+        return this.accept;
+    }
+
+    /*
+    public User getGuest(){
+        return this.guest;
+    }
+
+    public GetCommunity(){
+        return this.community;
+    }
+     */
+
+    public void setAccept(boolean value){
+        this.accept = value;
+    }
+
+    public void acceptInvitation(){
+        setAccept(true);
+
+
+    }
+
+    public void rejectInvatation(){
+        setAccept(false);
+    }
+}
