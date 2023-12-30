39963d00359495686abc2b9d03568d6d008ce79d
Laura Keidann <laura.keidann@sap.com>
2023-12-30 10:54:29 -0300

    Add friendships

--- src/main/java/content/User.java
+++ src/main/java/content/User.java
@@ -1,13 +1,16 @@
 package content;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class User {
 
     private String name;
     private String email;
     private String password;
-    private ArrayList<User> friends;
+    private Set<User> friends;
+    private Set<User> pendingFriends;
     private HabitsList habits;
     private Productivity productivity;
 
@@ -15,6 +18,8 @@ public class User {
     public User(String email, String password) {
         this.email = email;
         this.password = password;
+        this.friends = new HashSet<>();
+        this.pendingFriends = new HashSet<>();
     }
 
     // Getters and Setters
@@ -24,6 +29,14 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public Set<User> getFriends() {
+        return friends;
+    }
+
+    public Set<User> getPendingFriends() {
+        return pendingFriends;
     }
 
     public void setEmail(String email) {
@@ -39,20 +52,36 @@ public class User {
     }
 
 
-    public boolean addFriend(User friend) {
-        // TBD
+    public void addFriend(User user) {
+        if (!this.friends.contains(user)){
+            this.friends.add(user);
+        }
+    }
+
+    public boolean inviteFriend(User user) {
+        if(friends.contains(user) || pendingFriends.contains(user)) { // Check if user is already a friend
+            return false;
+        }
+        this.pendingFriends.add(user);
         return true;
     }
 
-    public boolean removeFriend(User friend) {
-        // TBD
-        return true;
+    public void acceptFriendInvite(User user) {
+        if(this.pendingFriends.contains(user)) {
+            this.pendingFriends.remove(user);
+            this.addFriend(user);
+            user.addFriend(this); // Adds to both users
+        }
     }
 
-    public boolean inviteFriend(User friend) {
-        // TBD
-        return true;
+    public void removeFriend(User user) {
+        if(this.friends.contains(user)) {
+            this.friends.remove(user); 
+            user.getFriends().remove(this); // Removes friendship from both users
+        }
     }
+
+
 
     public void createCommunity(String communityName, String owner) {
         return new Community(communityName, this.name); 
@@ -60,12 +89,10 @@ public class User {
 
 
     public void createList() {
+            // TBD
     }
-    // TBD
 
-    public boolean createUser() {
-    }
-    // TBD
+
 
     public Report generateProductivityReport() {
         return new Report(productivity);
