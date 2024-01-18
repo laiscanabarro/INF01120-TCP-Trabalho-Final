467a5f5c6728e8777ed0e3037a4e25afc9b8b078
Lais Canabarro <laisaraujo1312@gmail.com>
2024-01-18 16:45:04 -0300

    Fixes tests: Recurrence, RecurrenceList

--- src/test/java/content/RecurrenceTest.java
+++ src/test/java/content/RecurrenceTest.java
@@ -1,15 +1,21 @@
 package content;
 
 import org.junit.jupiter.api.*;
+
+import utils.Recurrence;
+import utils.Recurrence.RecurrenceType;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class RecurrenceTest {
-    boolean daily;
-    boolean weekday;
-    boolean weekly;
-    boolean monthly;
-    boolean annually;
-    boolean custom;
+    Recurrence recurrence;
+    ArrayList<TaskList> taskLists;
+    Calendar calendar;
+    LocalDate date;
+    Event event;
     
     @BeforeAll
     public static void start(){
@@ -18,32 +24,55 @@ public class RecurrenceTest {
     
     @BeforeEach
     public void init(){
-        
+        recurrence = new Recurrence();
+        taskLists = new ArrayList<>();
+        calendar = new Calendar(taskLists);
+        date = LocalDate.of(2024, 1, 1);
+        event = new Event("test", date);
     }
     
     @Test
     public void getRecurrenceType(){
-        
+        recurrence.setWeekly(true);
+        RecurrenceType type = recurrence.getRecurrenceType();
+        assertEquals(type, RecurrenceType.WEEKLY);
     }
     
     @Test
     public void verifyRecurrence(){
-        
+        recurrence.setAnnually(true);
+        boolean value = recurrence.verifyRecurrence();
+        assertEquals(value, true);
+
+        recurrence.setAnnually(false);
+        value = recurrence.verifyRecurrence();
+        assertEquals(value, false);
     }
     
     @Test
     public void toStringTest(){
-        
+        recurrence.setMonthly(true);
+        String type = recurrence.toString();
+        assertEquals(type, "monthly");
     }
     
     @Test
     public void changeRecurrence(){
-        
+        recurrence.setAnnually(true);
+        RecurrenceType type = RecurrenceType.DAILY;
+        recurrence.changeRecurrence(type, calendar, event);
+        assertEquals(recurrence.getAnnualy(), false);
+        assertEquals(recurrence.getDaily(),  true);
     }
     
     @Test
     public void clear(){
-        
+        recurrence.setDaily(true);
+        recurrence.setWeekday(true);
+        recurrence.clear(calendar, event);
+        assertEquals(recurrence.getDaily(), false);
+        assertEquals(recurrence.getWeekday(), false);
+        assertEquals(recurrence.verifyRecurrence(), false);
     }
     
     @AfterAll
